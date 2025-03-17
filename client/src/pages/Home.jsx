16a256de3dc4fd73cef704751d1ebd8bbf83e70f
Kevin XIE <kevin.xie1@ucalgary.ca>
2025-03-17 01:04:45 -0600

    updated home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,11 +3,20 @@ import React from 'react'
 const Home = () => {
   return(
     <div>
-      <h1>Welcome to Card Pairs</h1>
-      <p>A game of memory</p>
-      <a href = "/login">Login</a>
-      <a href = "/create">Create Game</a>
-      <a href = "/join">Join Game</a>
+      <br/>
+      <div class = "home-login-button">
+        <a href = "/login" id = "input-button">Login</a>
+      </div>
+      <div class = "home-introduction">
+        <h1 class = "home-title">Welcome to Card Pairs</h1>
+        <p class = "home-subtitle">A game of memory</p>
+      </div>
+      <div class = "home-buttons-list">
+        <a href = "/create" id = "input-button">Create Game</a>
+        <a href = "/join" id = "input-button">Join Game</a>
+        <a>Rules</a>
+      </div>
+    
     </div>
   )
 }
