b9b0e22969335ecba73d3f1dc923e0f0caf27ebf
Kevin XIE <kevin.xie1@ucalgary.ca>
2025-03-17 18:50:20 -0600

    finished first iteration cardgame GUI

--- client/src/components/CardGame.jsx
+++ client/src/components/CardGame.jsx
@@ -1,18 +1,51 @@
-import React from 'react';
+import {useState, useRef} from 'react';
 
-const items = [1,2,3,4,5];
-const suits = ["♠", "♣", "♥", "♦"];
+const values = ["♠", "♣", "♥", "♦", "🃏"];
 
-const deck = ([...suits, ...suits]);
-
-const handleClick = () => {};
+//create a deck of cards and shuffle them
+const deck = ([...values, ...values].sort(() => 0.5 - Math.random()));
+const defaultState = {index: null, value: null};
 
 function CardGame() { 
+  const [firstPick, setFirstPick] = useState(defaultState);
+  const [secondPick, setSecondPick] = useState(defaultState);
+  const [remainingCards, setRemainingCards] = useState(deck);
+  const [moves, setMoves] = useState(0);
+
+  const handleClick = (index, value) => {
+    //selecting the first card or selecting the first card after wrong pair match
+    if (firstPick.index === null || (firstPick.index !== null && secondPick.index !== null)) {
+      setSecondPick(defaultState);
+      setFirstPick({index, value});
+      setMoves((moves) => moves + 1);
+    }
+    //selecting the second card
+    else if (secondPick.index === null && firstPick.index !== index) {
+      setSecondPick({index, value});
+      setMoves((moves) => moves + 1);
+
+      //compare the two cards
+      //if there is a pair
+      if (firstPick.value === value) {
+        setRemainingCards(remainingCards.filter((card) => card !== value));
+      }
+    }
+  };
+
   return (
     <div className='cardgame-display'>
       {deck.map((card, index) => {
         return ( 
-          <div key={index} className='cardgame-card' onClick={handleClick}>
+          <div 
+            key={index} 
+            className={`cardgame-card 
+            ${(firstPick.index === index 
+              || secondPick.index === index 
+              || !remainingCards.includes(card)
+            ) && "cardgame-flipped"
+          }`}
+           onClick={() => handleClick(index, card)}
+           >
             <div className='cardgame-card-back'></div>
             {card}
           </div>
