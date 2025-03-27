172ffedd6720e9c834c25e25b0f48053de8bc4f6
Kevin XIE <kevin.xie1@ucalgary.ca>
2025-03-27 13:07:44 -0600

    added rules in the rules popup

--- client/src/components/DaRules.jsx
+++ client/src/components/DaRules.jsx
@@ -10,6 +10,39 @@ const DaRules = ({showRules, setShowRules}) => {
                 <h1>Rules</h1>
                 <div>
                     <h2>SinglePlayer</h2>
+                    <p>The object of the game is to win the most pairs (2
+                        identical cards). </p>
+                    <p>   
+                        If you turn over two identical cards,
+                        the card remains turned over and you score a point. 
+                    </p>
+                    <p>
+                        If you turn over two different cards, the cards will be 
+                        reset and you will NOT score a point.
+                    </p>
+                    <p>
+                        The game ends when all pairs have been found.
+                    </p>
+                </div>
+                <div>
+                    <h2>Multiplayer</h2>
+                    <p>The object of the game is to find the most pairs (2
+                        identical cards). </p>
+                    <p>The first turn will always go to the host player. </p>
+                    <p>Each player takes turns flipping over
+                        two cards. </p>
+                    <p>   
+                        If you turn over two identical cards,
+                        the card remains turned over and you score a point. 
+                    </p>
+                    <p>
+                        If you turn over two different cards, the cards will be 
+                        reset and you will NOT score a point.
+                    </p>
+                    <p>
+                        The game ends when all pairs have been found and the player with the highest
+                        score will be the winner.
+                    </p>
                 </div>
             </div>
         </body>
