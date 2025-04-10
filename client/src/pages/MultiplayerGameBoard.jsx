9600356a419570f131f217706883bb66d7e01e0c
Kevin XIE <kevin.xie1@ucalgary.ca>
2025-04-09 19:23:59 -0600

    call me the OFF brand cuz i be smashing bugs except ONE!!!!!

--- client/src/pages/MultiplayerGameBoard.jsx
+++ client/src/pages/MultiplayerGameBoard.jsx
@@ -1,38 +1,54 @@
-import React from 'react';
+import React, { use, useState } from 'react';
 import CardGame from '../components/CardGame';
 import GameProfile from '../components/GameProfile';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import MultiplayerWait from '../components/MultiplayerWait';
+import {useEffect} from 'react';
 
 const MultiplayerGameBoard = () => {
     const roomName = useParams();
     const gamestate = useSelector((state) => state.game.gameState);
-    const maxPlayers = gamestate.maxPlayers;
-    const currentPlayerTurn = gamestate.players[gamestate.currentTurnIndex].username;
-
-    function getGameStyle() {
+    console.log("Game state: ", gamestate);
+    const [maxPlayers, setMaxPlayers] = useState("");
+    const [currentPlayerTurn, setCurrentPlayerTurn] = useState("");
+    const [style,setStyle] = useState("");
+    const [players,setPlayers] = useState([]);
+    useEffect(() => {
+        setMaxPlayers(gamestate.maxPlayers);
+        setCurrentPlayerTurn(gamestate.players[gamestate.currentTurnIndex].username);
         if(gamestate.gamestyle === "Red & Gold") {
             console.log("Red & Gold style selected")
-            return "redgold";
+            setStyle("redgold");
         } else if (gamestate.gamestyle === "Purple & Yellow") {
-            return "purpleyellow";
+            setStyle ("purpleyellow");
         } else {
-            return "";
+            setStyle("");
         }
+        setPlayers(gamestate.players);
+    },[gamestate])
+    // function getGameStyle() {
+    //     if(gamestate.gamestyle === "Red & Gold") {
+    //         console.log("Red & Gold style selected")
+    //         return "redgold";
+    //     } else if (gamestate.gamestyle === "Purple & Yellow") {
+    //         return "purpleyellow";
+    //     } else {
+    //         return "";
+    //     }
 
-    }
+    // }
 
     return (
-        <div id = {getGameStyle()}>
+        <div id = {style}>
           <div className='gameboard-game-page'>
-            <MultiplayerWait currentPlayers = {gamestate.players.length} totalPlayers={maxPlayers}/>
             <div className='gameboard-cardgame'>
                 <CardGame roomName={roomName} />
             </div>
+            <MultiplayerWait currentPlayers = {players.length} totalPlayers={maxPlayers}/>
             {/* {console.log("Initial game state using dispatch: ", gamestate)} */}
             <div className='gameboard-userboard'>
-                {gamestate.players.map((player, i) => (
+                {players.map((player, i) => (
                     <GameProfile key={i} playerName={player.username} points={player.score} currentPlayerTurn = {currentPlayerTurn}/>
                 ))}
             </div>
